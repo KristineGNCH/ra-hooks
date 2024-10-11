2c5b2ee2f2dbe990fe5d0e6e782971f5c0f2d48d
KristineGNCH <kristine.gnch@gmail.com>
2024-10-11 22:00:15 +0300

    second commit

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 
 export const Details = ({id}) => {
@@ -32,7 +33,7 @@ export const Details = ({id}) => {
       ) : user ? (
         <>
           <div className="details__name">{user.name}</div>
-          <img src={`${user.photo}`} alt="image" className="main__photo" />
+          <img src={`${user.avatar}`} alt="image" className="avatar__details" />
           <p className="item city__details">{user.details.city}</p>
           <p className="item company__details">{user.details.company}</p>
           <p className="item position__details">{user.details.position}</p>
